f7349d2020c8d44350235c3b835b546ecf80a658
AdityaaPurohitt <102147885+AdityaaPurohitt@users.noreply.github.com>
2022-09-12 15:09:57 +0530

    for payment model added

---
+++ app/Models/ArtPayment.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ArtPayment extends Model
+{
+    protected $table   = "art_payments";
+    protected $guarded = ['id'];
+}
