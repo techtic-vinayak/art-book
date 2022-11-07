ea4995f8df42f7c9faaf87055a0c612c036c4c67
techtic-suresh <naman@techtic.agency>
2022-11-07 14:50:44 +0530

    client test credential changes

--- config/constants.php
+++ config/constants.php
@@ -1,9 +1,22 @@
 <?php
+
+// test credentials
+// return [
+	
+// 	'strip'=> [
+//         'CLIENT_ID'     => "ca_MQ0mOaoCv7Iup2rTMaatJDPjCvtOwvv4",
+//         'STRIPE_KEY'    => "pk_test_51LhAPLBaboD5EK2xkeQYfhRNk882UanNiBBot6yviyDgZCKU4Yw6Sdym3lqlKTYtJTouDaiSVtzqDnYb4IIDU84s00i0MXmp5z",
+//         'STRIPE_SECRET' => "sk_test_51LhAPLBaboD5EK2xw5seeY4LA4efxCbO7Sl27HD3Rz973aSEK6uLNOn8vW9UEqAYPwM9LxE1lbktXoKhXc2dWuSp00SWDtJdix", 
+//     ]
+// ];
+
+// client-test credentials
+
 return [
 	
 	'strip'=> [
-        'CLIENT_ID'     => "ca_MQ0mOaoCv7Iup2rTMaatJDPjCvtOwvv4",
-        'STRIPE_KEY'    => "pk_test_51LhAPLBaboD5EK2xkeQYfhRNk882UanNiBBot6yviyDgZCKU4Yw6Sdym3lqlKTYtJTouDaiSVtzqDnYb4IIDU84s00i0MXmp5z",
-        'STRIPE_SECRET' => "sk_test_51LhAPLBaboD5EK2xw5seeY4LA4efxCbO7Sl27HD3Rz973aSEK6uLNOn8vW9UEqAYPwM9LxE1lbktXoKhXc2dWuSp00SWDtJdix", 
+        'CLIENT_ID'     => "ca_MkxCw1szJ5C6DJg1LHC8g7G0NysuMkRm",
+        'STRIPE_KEY'    => "pk_test_51LqmbbG2aSXPVlWHVO1PysfAc0XjZFVvfNAoVDCrUcy3XnclbWNJoZ4JjpgS24aQdxVe3ENVoqsRcGvncm10Rd6800uHWc9zgy",
+        'STRIPE_SECRET' => "sk_test_51LqmbbG2aSXPVlWHm3VHWF8aQiKMlpP2nS08ZY3AkWqv5p0t1FCDV7iSj3LWMK1Cvr4CHgbtZDNFXrDQ9MfutSR400GmDpwhaz ", 
     ]
 ];
